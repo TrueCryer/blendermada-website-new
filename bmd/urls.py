ac7c894f65cd9706919e6d148aeb95f6a7a8ca75
OZEROV Sergey <sergey.ozerov@renault.com>
2022-07-01 16:59:10 +0300

    Add media URLs

--- bmd/urls.py
+++ bmd/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +27,10 @@ urlpatterns = [
 
     path('about/', TemplateView.as_view(template_name='about.html'), name='about')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
