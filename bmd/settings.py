73075dd27f6a0e5a9922c37a549fef1c14aff76d
OZEROV Sergey <sergey.ozerov@renault.com>
2022-07-01 16:58:47 +0300

    Add media settings

--- bmd/settings.py
+++ bmd/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.parent / '_files' / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.parent / '_files' / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
